e0de4d324efe43f14be77934a2b235ea77bf0bdf
장교준 <wkddn2003@gmail.com>
2025-02-13 15:41:36 +0900

    데이터 검증

--- src/main/java/com/fastcampus/ch4/practice/UserValidator.java
+++ src/main/java/com/fastcampus/ch4/practice/UserValidator.java
@@ -1,13 +1,11 @@
 package com.fastcampus.ch4.practice;
 
-
 import org.apache.catalina.User;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
-public class UserValidator2 implements Validator {
-
+public class UserValidator implements Validator {
 
 
     @Override
@@ -17,12 +15,16 @@ public class UserValidator2 implements Validator {
 
     @Override
     public void validate(Object target, Errors errors) {
-
         User user = (User) target;
+        
+//               String id = user.getId();
 
+        ValidationUtils.rejectIfEmpty(errors, "id", "id.required");
+        ValidationUtils.rejectIfEmpty(errors, "pwd", "pwd.required");
 
-        ValidationUtils.rejectIfEmpty(errors,"id","id.required");
-        ValidationUtils.rejectIfEmpty(errors,"pwd","pwd.required");
+//        if(id==null || id.length() < 5 || id.length() > 12) {   // id길이 체크
+//            errors.rejectValue("id", "id.invalid");
+//        }
+
     }
-
 }
